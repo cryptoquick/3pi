8702d492da3855673762983bec47327b1ea2e7d2
Hunter Trujillo <cryptoquick@gmail.com>
2018-06-05 13:39:10 -0600

    Add list documents test.

--- src/index.test.ts
+++ src/index.test.ts
@@ -5,18 +5,29 @@ import * as faker from 'faker'
 faker.seed(314)
 
 test('insert a document', async t => {
-  t.is(
-    (await request.post({
-      baseUrl: 'http://localhost:3000/api',
-      uri: '/doc',
-      body: {
-        title: faker.name.title(),
-        author: faker.name.findName(),
-        content: faker.lorem.paragraph(),
-      },
-      json: true,
-      resolveWithFullResponse: true,
-    })).statusCode,
-    200
-  )
+  const res = await request.post({
+    baseUrl: 'http://localhost:3000/api',
+    uri: '/doc',
+    body: {
+      title: faker.name.title(),
+      author: faker.name.findName(),
+      content: faker.lorem.paragraph(),
+    },
+    json: true,
+    resolveWithFullResponse: true,
+  })
+
+  t.is(res.statusCode, 200)
+})
+
+test('lists documents', async t => {
+  const results = await request.get({
+    baseUrl: 'http://localhost:3000/api',
+    uri: '/doc',
+    json: true,
+  })
+
+  console.log(results)
+
+  t.not(results.length, 0)
 })
